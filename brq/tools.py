09983a2280420b56594dc3eecf0111e998108433
Zhongsheng Ji <9573586@qq.com>
2024-06-06 16:27:15 +0800

    Support username and password in url (#5)
    * Support username and password in url
    
    * Update readme

--- brq/tools.py
+++ brq/tools.py
@@ -42,12 +42,17 @@ def get_redis_url(
     db: int = 0,
     cluster: bool = False,
     tls: bool = False,
+    username: str = "",
+    password: str = "",
 ):
     if not cluster:
         url = f"{host}:{port}/{db}"
     else:
         logger.debug("Cluster mode, no need to specify db")
         url = f"{host}:{port}"
+
+    if username or password:
+        url = f"{username}:{password}@{url}"
 
     if tls:
         return f"rediss://{url}"
